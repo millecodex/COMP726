791e28bef00c2a8dfa526c7113e1387ab9ae3f62
millecodex <39792005+millecodex@users.noreply.github.com>
2020-09-21 18:08:17 +1200

    Update helloWorld.sol

--- Tutorial_8/helloWorld.sol
+++ Tutorial_8/helloWorld.sol
@@ -4,13 +4,8 @@
 pragma solidity ^0.7.1;
 
 contract helloWorld{
-//
-int balance;
-int principle = 100;
-uint rate = 5;
-//global variables are more $$$
-uint comp;
-int timePeriods2;
+//global variables here; these are more $$$
+uint value;
 
     /* Remix button colours:
      * blue buttons are for pure/view; 
@@ -19,48 +14,30 @@ int timePeriods2;
      * also red are payable (next week)
      */
     function hello() public pure returns(string memory){
-        return 'helloWorld';
+        //this function call should be free; check gas
+        return 'hello there amigo';
     }
     
-    //look at the gas cost here -> 21850 every time its called!
-    //see the ether balance leftover in the account
-    function getBalance() public view returns(int){
-        return balance;
+    //this function will do some calculation
+    //note the button colour
+    //remix will tell you exactly how much gas (21045)
+    function helloGas() public returns(uint){
+        return value++;
     }
     
-    /*function calcInterest() public returns(int interest){
-        interest = principle*rate/100;
-        balance = principle + interest;
-        return interest;
-    }*/
-    //compound interest means that at time t+1 the interest
-    //is calculated on balance + interest at time time
-    // balance = principle*(1 - rate^timePeriods)
-    // so for 1 year of compounded monthly @5%:
-    // balance = 100*(1+5/100)^12
-    /*function compInterest() public returns(uint comp){
-        //base2 = 1+rate/100;
-        //timePeriods2 = 12;
-        //compInterest = base2^timePeriods2;
-        //use local variables
-        uint base = 1+rate/100;
-        uint timePeriods = 124;
-        comp = base**timePeriods;
-        return comp;
-    }*/
-    
-    function compInterest() public {
-        //base2 = 1+rate/100;
-        //timePeriods2 = 12;
-        //compInterest = base2^timePeriods2;
-        //use local variables
-        uint base = 1+rate;
-        uint timePeriods = 12;
-        comp = base**timePeriods;
+    //we have 10 VM accounts here, lets look at a balance
+    /*globally available variables: https://solidity.readthedocs.io/en/v0.7.1/units-and-global-variables.html
+     * msg.sender
+     * msg.value
+     * tx.gasprice
+     * block.number
+       etc.*/ 
+    function getBalance() public view returns(uint){
+        //this is the contract balance, not the user account balance
+        return address(this).balance;
     }
-    
-    function getCompInterest() public view returns(uint){
-        return comp;
+    //How to output a user's (address) balance?
+    function getAccountBalance() public view returns(uint){
+    //code here
     }
-    
 }
