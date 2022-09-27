92a3273b6940755a3922e9a7045790b72f0caf71
Mille Codex <39792005+millecodex@users.noreply.github.com>
2022-09-27 15:04:56 +1300

    Add files via upload

--- my-nft-app/scripts/deploy.js
+++ my-nft-app/scripts/deploy.js
@@ -1,1 +1,16 @@
-init
+async function main() {
+    const CourseNFT = await ethers.getContractFactory("courseNFT")
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const courseNFT = await CourseNFT.deploy()
+    await courseNFT.deployed()
+    console.log("Contract deployed to address:", courseNFT.address)
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
+  
